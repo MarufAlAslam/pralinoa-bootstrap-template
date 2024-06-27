abdcafbf41705f55b5ea072549c75e50f8a3606c
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-06-28 00:17:57 +0600

    hero done

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    slidesToShow: 1,
+    slideToScroll: 1
+});
