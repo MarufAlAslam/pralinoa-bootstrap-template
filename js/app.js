6c32e920a9ed42fe56ec1a19854c59dc3f18fcf6
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-06-28 01:26:04 +0600

    homepage done

--- js/app.js
+++ js/app.js
@@ -9,3 +9,24 @@ $('.slider').slick({
     slidesToShow: 1,
     slideToScroll: 1
 });
+
+
+$('.dual-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    slidesToShow: 2,
+    slideToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
